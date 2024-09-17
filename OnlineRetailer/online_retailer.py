11279cd27fac5b11ebe849639fa1a004c9544fae
htodorov0000 <htodorov0000@gmail.com>
2024-09-17 18:49:28 +0300

    inherited menu classes

--- OnlineRetailer/online_retailer.py
+++ OnlineRetailer/online_retailer.py
@@ -1,42 +1,73 @@
-INVALID_INPUT: str = "Invalid input. Try again."
-
 class NumberedMenu:
-    user_input: int
-    def __init__(self, name, menu_items):
+    INVALID_INPUT: str = "Invalid input type. Try again."
+    INVALID_RANGE: str = "Please input a number in the correct range."
+    def __init__(self, name):
         self.name = name
-        self.menu_items = menu_items
+        self.user_input: int
+        self.menu_items = []
+    
+    def start(self):
+        self.draw_menu()
 
     def draw_menu(self):
         menu_string = ""
         for num, menu_item in enumerate(self.menu_items):
-            menu_string += (str(num) + ". " + menu_item + """
+            menu_string += (str(num + 1) + ". " + menu_item + """
 """)
         while True:
             try:
                 self.user_input = int(input(menu_string))
-                if self.user_input < len(self.menu_items):
+                if self.user_input < len(self.menu_items) and self.user_input > 0:
                     break
                 else:
-                    print(INVALID_INPUT)
-            except:
-                print("Please input a number in the correct range.")
-        print(self.menu_items[self.user_input])
+                    print(self.INVALID_RANGE)
+            except ValueError:
+                print(self.INVALID_INPUT)
+        self.apply_chosen_option()
+                
+    def apply_chosen_option(self):
+        pass
+    
+class SettingBoolMenu(NumberedMenu):   
+    def __init__(self, name):
+        self.menu_items = []
+        self.setting: int
+        return super().__init__(name)
+
+    def apply_chosen_option(self):
+        if self.user_input == 1:
+            self.setting = True
+        elif self.user_input == 2:
+            self.setting = False
         
+class NavigationMenu(NumberedMenu):
+    def __init__(self, name):
+        self.menus = []
+        return super().__init__(name)
+    
+    def start(self):
+        menu_name_array = []
+        for menu in self.menus:
+            menu_name_array.append(menu.name)
+        self.menu_items = menu_name_array
+        self.draw_menu()
+        
+    def apply_chosen_option(self):
+        self.menus[self.user_input - 1].start()
+            
 
-security_menu = NumberedMenu("Security Menu", ["Security ON", "Security OFF"])
-security_menu.draw_menu()
+security_menu = SettingBoolMenu("Security Menu")     
+test_nav_menu1 = NavigationMenu("TestNavMenu1")
+test_nav_menu2 = NavigationMenu("TestNavMenu2")
+test_nav_menu3 = NavigationMenu("TestNavMenu3")
+test_nav_menu4 = NavigationMenu("TestNavMenu4")
 
-# while True:
-#     security_input = input("""1. Security On
-# 2. Security Off
-# """)
-#     if security_input == "1":
-#         security = True
-#         print("Security mode ON")
-#         break
-#     if security_input == "2":
-#         security = False
-#         print("Security mode OFF")
-#         break
-#     print(INVALID_INPUT)
+security_menu.menu_items = ["Security ON", "Security OFF"]
+test_nav_menu1.menus = [test_nav_menu2, test_nav_menu3, test_nav_menu4]
+test_nav_menu2.menus = [test_nav_menu1, test_nav_menu3, test_nav_menu4]
+test_nav_menu3.menus = [test_nav_menu1, test_nav_menu2, test_nav_menu4]
+
+security_menu.start()
+print(security_menu.setting)
+test_nav_menu1.start()
 
