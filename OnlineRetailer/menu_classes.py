37de56944bea7262899920387d601780f6ce6e6f
htodorov0000 <htodorov0000@gmail.com>
2024-09-22 19:10:02 +0300

    touching up menu classes

--- OnlineRetailer/menu_classes.py
+++ OnlineRetailer/menu_classes.py
@@ -1,10 +1,12 @@
 class NumberedMenu:
     INVALID_INPUT: str = "Invalid input type. Try again."
     INVALID_RANGE: str = "Please input a number in the correct range."
-    def __init__(self, name):
+    def __init__(self, name, description):
         self.name = name
+        self.description = description
         self.user_input: int
         self.menu_items = []
+        self.options = []
     
     def start(self):
         self.draw_menu()
@@ -12,19 +14,21 @@ class NumberedMenu:
     def draw_menu(self):
         menu_string = ""
         for num, menu_item in enumerate(self.menu_items):
-            menu_string += (str(num + 1) + ". " + menu_item + """
-""")
+            menu_string += (str(num + 1) + ". " + menu_item) + """
+"""
         while True:
             print()
             print("***" + self.name.upper() + "***")
             print()
+            if self.description != "": #Print description if it isn't empty.
+                print(self.description)
+                print()
             try:
                 self.user_input = int(input(menu_string))
                 if self.user_input < len(self.menu_items) + 1 and self.user_input > 0:
                     break
-                else:
-                    print(self.INVALID_RANGE)
-                    print()
+                print(self.INVALID_RANGE)
+                print()
             except ValueError:
                 print(self.INVALID_INPUT)
                 print()
@@ -35,10 +39,10 @@ class NumberedMenu:
         pass
     
 class SettingBoolMenu(NumberedMenu):   
-    def __init__(self, name):
+    def __init__(self, name, description):
         self.menu_items = []
         self.setting: int
-        super().__init__(name)
+        super().__init__(name, description)
 
     def apply_chosen_option(self):
         if self.user_input == 1:
@@ -47,16 +51,15 @@ class SettingBoolMenu(NumberedMenu):
             self.setting = False
         
 class NavigationMenu(NumberedMenu):
-    def __init__(self, name):
-        self.menus = []
-        super().__init__(name)
+    def __init__(self, name, description):
+        super().__init__(name, description)
     
     def start(self):
         menu_name_array = []
-        for menu in self.menus:
+        for menu in self.options:
             menu_name_array.append(menu.name)
         self.menu_items = menu_name_array
         self.draw_menu()
         
     def apply_chosen_option(self):
-        self.menus[self.user_input - 1].start()
+        self.options[self.user_input - 1].start()
--- OnlineRetailer/online_retailer.py
+++ OnlineRetailer/online_retailer.py
@@ -1,22 +1,18 @@
-from menu_classes import NumberedMenu
 from menu_classes import SettingBoolMenu
 from menu_classes import NavigationMenu
-            
+
+DEFAULT_DESCRIPTION = "Please select the desired menu."
+
 #Declare menu objects:
 
-security_menu = SettingBoolMenu("Security Mode")     
-landing_menu = NavigationMenu("Landing")
-test_nav_menu2 = NavigationMenu("TestNavMenu2")
-test_nav_menu3 = NavigationMenu("TestNavMenu3")
-test_nav_menu4 = NavigationMenu("TestNavMenu4")
+security_menu = SettingBoolMenu("Security Mode", "Please select the desired security setting.")     
+landing_menu = NavigationMenu("Landing", DEFAULT_DESCRIPTION)
 
 #Define menu paths:
 
 security_menu.menu_items = ["Security ON", "Security OFF"]
-landing_menu.menus = [test_nav_menu2, test_nav_menu3, test_nav_menu4]
-# test_nav_menu2.menus = [test_nav_menu1, test_nav_menu3, test_nav_menu4]
-# test_nav_menu3.menus = [test_nav_menu1, test_nav_menu2, test_nav_menu4]
-# test_nav_menu4.menus = [test_nav_menu1, test_nav_menu2, test_nav_menu3]
+landing_menu.options = [security_menu]
+
 
 #Security:
 security_menu.start()
@@ -25,4 +21,4 @@ security = security_menu.setting
 #TODO: Change settings depending on security here
 
 #Start:
-test_nav_menu1.start()
+landing_menu.start()
