36897591ee4c089f9d2db6b393733126dbeedd2d
htodorov0000 <htodorov0000@gmail.com>
2024-09-23 03:21:11 +0300

    added text input menus

--- OnlineRetailer/menu_classes.py
+++ OnlineRetailer/menu_classes.py
@@ -1,15 +1,55 @@
-class NumberedMenu:
-    INVALID_INPUT: str = "Invalid input type. Try again."
-    INVALID_RANGE: str = "Please input a number in the correct range."
+class Menu:
     def __init__(self, name, description):
         self.name = name
         self.description = description
-        self.user_input: int
-        self.menu_items = []
-        self.options = []
     
     def start(self):
         self.draw_menu()
+    
+    def draw_name_and_description(self):
+        print()
+        print("***" + self.name.upper() + "***")
+        print()
+        if self.description != "": #Print description if it isn't empty.
+            print(self.description)
+            print()
+
+    def draw_menu(self):
+        self.draw_name_and_description()
+
+class TextInputMenu(Menu):
+    def __init__(self, name, description, input_queries):
+        self.input_queries = input_queries
+        self.user_input = []
+        super().__init__(name, description)
+
+    def draw_menu(self):
+        while True:
+            self.draw_name_and_description()
+            for num, query in enumerate(self.input_queries):
+                user_input = input(query + ": ")
+                self.user_input.append(user_input)
+            self.apply_user_input()
+
+    def apply_user_input(self):
+        pass
+
+class RegistrationMenu(TextInputMenu):
+    def apply_user_input(self):
+        pass #write username and password
+    
+class LoginMenu(TextInputMenu):
+    def apply_user_input(self):
+        pass #check username and password
+                
+class NumberedMenu(Menu):
+    INVALID_INPUT: str = "Invalid input type. Try again."
+    INVALID_RANGE: str = "Please input a number in the correct range."
+    def __init__(self, name, description):
+        self.user_input: int
+        self.menu_items = []
+        self.options = []
+        super().__init__(name, description)
 
     def draw_menu(self):
         menu_string = ""
@@ -17,12 +57,7 @@ class NumberedMenu:
             menu_string += (str(num + 1) + ". " + menu_item) + """
 """
         while True:
-            print()
-            print("***" + self.name.upper() + "***")
-            print()
-            if self.description != "": #Print description if it isn't empty.
-                print(self.description)
-                print()
+            self.draw_name_and_description()
             try:
                 self.user_input = int(input(menu_string))
                 if self.user_input < len(self.menu_items) + 1 and self.user_input > 0:
--- OnlineRetailer/online_retailer.py
+++ OnlineRetailer/online_retailer.py
@@ -1,24 +1,31 @@
-from menu_classes import SettingBoolMenu
+from menu_classes import LoginMenu, RegistrationMenu, SettingBoolMenu
 from menu_classes import NavigationMenu
+from menu_classes import TextInputMenu
 
 DEFAULT_DESCRIPTION = "Please select the desired menu."
-
+SECURITY_DESCRIPTION = "Please select the desired security setting."
+REGISTRATION_DESCRIPTION = "Please create a new username and password."
+LOGIN_DESCRIPTION = "Please input your username and password."
 #Declare menu objects:
 
-security_menu = SettingBoolMenu("Security Mode", "Please select the desired security setting.")     
+security_menu = SettingBoolMenu("Security Mode", SECURITY_DESCRIPTION)     
 landing_menu = NavigationMenu("Landing", DEFAULT_DESCRIPTION)
+registration_menu = RegistrationMenu("Sign Up", REGISTRATION_DESCRIPTION, ["Username" , "Password"])
+login_menu = LoginMenu("Login", LOGIN_DESCRIPTION, ["Username" , "Password"])
+
 
 #Define menu paths:
-
 security_menu.menu_items = ["Security ON", "Security OFF"]
-landing_menu.options = [security_menu]
+landing_menu.options = [registration_menu, login_menu]
 
 
 #Security:
 security_menu.start()
 security = security_menu.setting
 
-#TODO: Change settings depending on security here
+#TODO: Change settings depending on security and login here
+
+
 
 #Start:
 landing_menu.start()
