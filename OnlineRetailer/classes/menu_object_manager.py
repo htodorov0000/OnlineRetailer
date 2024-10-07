84b28bff9f4b896936d7481a2484d251b710ed25
htodorov0000 <htodorov0000@gmail.com>
2024-10-08 02:56:37 +0300

    menu object manager tweak

--- OnlineRetailer/classes/menu_object_manager.py
+++ OnlineRetailer/classes/menu_object_manager.py
@@ -7,10 +7,12 @@ class MenuObjectManager():
     PRODUCTS_DESCRIPTION = "Please select the desired product."
     REGISTRATION_DESCRIPTION = "Please create a new username and password."
     LOGIN_DESCRIPTION = "Please input your username and password."
+    LOGOUT_DESCRIPTION = "Are you sure you want to log out?"
     
     def __init__(self, account_manager, database_manager):
         self.security_menu = SettingBoolMenu("Security Mode", self.SECURITY_DESCRIPTION, account_manager)     
         self.landing_menu = NavigationMenu("Landing", self.DEFAULT_DESCRIPTION, account_manager)
+        self.logout_menu = SettingBoolMenu("Log Out", self.LOGOUT_DESCRIPTION, account_manager)
         self.products_menu = NavigationMenu("Products", self.PRODUCTS_DESCRIPTION, account_manager)
         self.product1_menu = NavigationMenu("Product1", "" , account_manager)
         self.product2_menu = NavigationMenu("Product2", "" , account_manager)
@@ -21,9 +23,10 @@ class MenuObjectManager():
         #Define menu paths:
         self.security_menu.menu_items = ["Security ON", "Security OFF"]
         self.landing_menu.options = [self.registration_menu, self.login_menu]
+        self.logout_menu.menu_items = ["Yes, No"]
         self.products_menu.options = [self.landing_menu, self.product1_menu, self.product2_menu]
         self.product1_menu.options = [self.landing_menu]
-        self.product2_menu.options = [self.landing_menu]        
+        self.product2_menu.options = [self.landing_menu]
 
     def guest_menu_options(self):
         self.landing_menu.options = [self.registration_menu, self.login_menu]
